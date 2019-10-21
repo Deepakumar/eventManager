c781bf79ceabced7fc12cf1a62dc6e0c11f05e7e
Deepakumar <gdeepakumar@mail.com>
2019-10-20 18:30:37 -0700

    Comm with chilm comm

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,27 +1,11 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:'event-list',
-    template: `
-                <div>
-                    <h1>Upcoming Events</h1>
-                    <hr/>
-                    <div class="well hoverwell thumbnail">
-                        <h2>{{event.name}}</h2>
-                        <div>Date: {{event.date}}</div>
-                        <div>Time:{{event.time}}</div>
-                        <div>Price: \$ {{event.price}}</div>
-                        <div>
-                            <span>Location: {{event.location.address}}</span>
-                            <span>&nbsp;</span>
-                            <span>{{event.location.city}}, {{event.location.country}}</span>
-                        </div>
-                    </div>
-                </div>
-              `
+    selector: 'event-list',
+    templateUrl: './events-list.component.html'
 })
 export class  EventListComponent {
-    event = {
+    eventData = {
         id:1,
         name:'Angular connect',
         date: '9/26/2019',
