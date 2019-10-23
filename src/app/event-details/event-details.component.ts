e6bf13adcc54fe1ecd6750e5225347529a2be271
Deepakumar <gdeepakumar@gmail.com>
2019-10-23 15:26:10 +0300

    Link to Route

--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EventserviceService } from '../eventservice.service';
+import { ActivatedRoute} from '@angular/router';
 
 @Component({
   templateUrl: './event-details.component.html',
@@ -7,10 +8,11 @@ import { EventserviceService } from '../eventservice.service';
 })
 export class EventDetailsComponent implements OnInit {
   event:any;
-  constructor(private eventService: EventserviceService) { }
+  constructor(private eventService: EventserviceService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.event = this.eventService.getEvent(1);
+    this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
+    console.log(this.route.snapshot.params['id']);
   }
 
 }
